ee0f2ccb81601a26c8ee5af1eac6098241ad1d87
pshitote@gmail.com <pshitote@gmail.com>
2023-11-19 23:39:16 +0300

    Local Storage ITask Interface

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,6 +1,6 @@
-import { Task } from "../redux/taskSlice";
+import { ITask } from "../../types";
 // Function to save tasks to localStorage
-export const saveTasksToLocalStorage = (tasks: Task[]) => {
+export const saveTasksToLocalStorage = (tasks: ITask[]) => {
   try {
     const serializedTasks = JSON.stringify(tasks);
     localStorage.setItem("tasks", serializedTasks);
